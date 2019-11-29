a6d3997b16fcd7a6c0e403fd6ad2d8de96c68761
niftylettuce <niftylettuce@gmail.com>
2019-11-28 19:32:29 -0600

    chore: bump deps

--- api.js
+++ api.js
@@ -1,17 +1,40 @@
-#!/usr/bin/env node
-const Server = require('@ladjs/api');
+const path = require('path');
+
+const API = require('@ladjs/api');
+const Axe = require('axe');
 const Graceful = require('@ladjs/graceful');
-const maxListenersExceededWarning = require('max-listeners-exceeded-warning');
-const Logger = require('@ladjs/logger');
-const Router = require('koa-router');
-const lipoKoa = require('lipo-koa');
-const multer = require('koa-multer');
+const Router = require('@koa/router');
 const bytes = require('bytes');
+const ip = require('ip');
+const lipoKoa = require('lipo-koa');
+const multer = require('@koa/multer');
+const pino = require('pino');
+const {Signale} = require('signale');
 
-if (process.env.NODE_ENV !== 'production') maxListenersExceededWarning();
+// note that we had to specify absolute paths here bc
+// otherwise tests run from the root folder wont work
+const env = require('@ladjs/env')({
+  path: path.join(__dirname, '.env'),
+  defaults: path.join(__dirname, '.env.defaults'),
+  schema: path.join(__dirname, '.env.schema')
+});
 
-const logger = new Logger({appName: 'lipo'});
-const routes = new Router();
+const router = new Router();
+const logger = new Axe({
+  showStack: env.SHOW_STACK,
+  name: env.APP_NAME,
+  level: 'debug',
+  capture: false,
+  logger:
+    env === 'production'
+      ? pino({
+          customLevels: {
+            log: 30
+          }
+        })
+      : new Signale()
+});
+
 const upload = multer({
   limits: {
     fileSize: bytes('20mb'),
@@ -22,17 +45,36 @@ const upload = multer({
   }
 });
 
-routes.post('/', upload.single('input'), lipoKoa);
+router.post('/', upload.single('input'), lipoKoa);
 
-const server = new Server({
-  routes,
+const api = new API({
+  routes: router,
   logger
 });
 
+console.log('api.config', api.config);
+
 if (!module.parent) {
-  server.listen(process.env.API_PORT || 3000);
-  const graceful = new Graceful({server, logger});
-  graceful.listen();
+  const graceful = new Graceful({
+    servers: [api],
+    redisClients: [api.client],
+    logger
+  });
+
+  (async () => {
+    try {
+      await Promise.all([api.listen(api.config.port), graceful.listen()]);
+      if (process.send) process.send('ready');
+      const {port} = api.server.address();
+      logger.info(
+        `Lad API server listening on ${port} (LAN: ${ip.address()}:${port})`
+      );
+    } catch (err) {
+      logger.error(err);
+      // eslint-disable-next-line unicorn/no-process-exit
+      process.exit(1);
+    }
+  })();
 }
 
-module.exports = server;
+module.exports = api;
